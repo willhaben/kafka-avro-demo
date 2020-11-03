e647e0fce20bb90307c008fc0eb26d6f45652120
Matthias Kausl <matthias.kausl@willhaben.at>
2020-11-03 13:13:55 +0100

    refactor

--- producer/src/main/java/avrokafkademo/services/Producer.java
+++ producer/src/main/java/avrokafkademo/services/Producer.java
@@ -1,7 +1,6 @@
 package avrokafkademo.services;
+
 import at.willhaben.tech.avro.User;
-import at.willhaben.tech.avro.UserBackwardCompatible;
-import at.willhaben.tech.avro.UserForwardCompatible;
 import org.apache.avro.specific.SpecificRecord;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
@@ -21,17 +20,12 @@ public class Producer {
     @Scheduled(fixedRate = 2000,initialDelay = 0)
     public void sendMessage() {
         final User user = new User("Matthias", "Kausl", "Happy " + System.currentTimeMillis(), 1);
-        final UserBackwardCompatible backwardCompatible = new UserBackwardCompatible("Matthias", "Happy " + System.currentTimeMillis(), "Java", 2);
-        final UserForwardCompatible forwardCompatible = new UserForwardCompatible("Matthias", "Kausl", "English", 3);
 
-        SpecificRecord data = forwardCompatible;
+        SpecificRecord data = user;
         logger.info(String.format("Producing -> %s", data));
 
         this.kafkaTemplate.send("someTopic", data);
     }
 
-    private String getRandomData(String prefix) {
-        return prefix + "/" + System.currentTimeMillis();
-    }
 
 }
